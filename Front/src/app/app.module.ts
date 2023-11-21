0a4ca76d5dab4203003b0552e7d31c04567d59df
Julian Sarmiento <julian.sarmiento.jeso.com>
2023-11-20 19:32:51 -0500

    generate intro component

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
