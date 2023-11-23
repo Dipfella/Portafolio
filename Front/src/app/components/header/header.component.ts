fa5d5281a7a8f9393696f2def451cd0d5736b0ce
Julian Sarmiento <julian.sarmiento.jeso.com>
2023-11-23 16:57:28 -0500

    Add funcionality header and other corrections

--- Front/src/app/components/header/header.component.ts
+++ Front/src/app/components/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  sections!: NodeListOf<HTMLElement>;
+  navLinks!: NodeListOf<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    document.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    this.sections = document.querySelectorAll('section');
+    this.navLinks = document.querySelectorAll('nav a');
+    const { scrollY } = window;
+
+    this.sections.forEach((section) => {
+      const { id, offsetTop, offsetHeight } = section;
+      const offset = offsetTop - 150;
+
+      if (scrollY >= offset && scrollY < offset + offsetHeight  ) {
+        this.navLinks.forEach((link) => link.classList.remove('active'));
+        this.navLinks.forEach((link) => {
+          if (link.dataset['scroll'] == id) {
+            link.classList.add('active');
+          }
+        });
+      }
+    });
+  }
 
 }
